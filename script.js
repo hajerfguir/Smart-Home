390b7606fbead4b848c728e0e465afed7029447f
hajerfguir <hajerfguir@gmail.com>
2025-03-20 16:03:03 -0400

    Initial commit - Smart Home UI

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("get-started").addEventListener("click", function() {
+    alert("Next Page Navigation Placeholder"); 
+    // Replace this with: window.location.href = "dashboard.html";
+});
